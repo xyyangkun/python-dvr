f08d3a5de6c06ffebcf6fad313ca654136ceac9e
NeiroNx <NeiroNx@users.noreply.github.com>
2018-03-10 18:50:34 +0300

    Add Alarm thread

--- dvrip.py
+++ dvrip.py
@@ -43,11 +43,15 @@ class DVRIPCam(object):
 		self.socket = None
 		self.packet_count = 0
 		self.session = 0
-		self.alive = None
 		self.alive_time = 20
+		self.alive = None
+		self.alarm = None
+		self.alarm_func = None
+		self.busy = threading.Condition()
 	def connect(self):
 		self.socket = socket(AF_INET, SOCK_STREAM)
 		self.socket.connect((self.ip, self.port))
+		self.socket.settimeout(.1)
 	def close(self):
 		self.alive.cancel()
 		self.socket.close()
@@ -55,19 +59,23 @@ class DVRIPCam(object):
 	def send(self, msg, data):
 		if self.socket == None:
 			return {"Ret":101}
+		#self.busy.wait()
+		self.busy.acquire()
 		if hasattr(data, '__iter__'):
 			data = json.dumps(data, ensure_ascii=False).encode('utf8')
 		self.socket.send(struct.pack('BB2xII2xHI',255, 0, self.session, self.packet_count, msg ,len(data)+2)+data+"\x0a\x00")
+		reply = {"Ret":101}
 		try:
 			head, version, self.session, sequence_number, msgid, len_data = struct.unpack('BB2xII2xHI',self.socket.recv(20))
 			sleep(.1)#Just for recive whole packet
 			reply = self.socket.recv(len_data)
 			self.packet_count += 1
 			reply = json.loads(reply[:-2],encoding="utf8")
-			return reply
 		except:
-			return {"Ret":101}
-		
+			pass
+		finally:
+			self.busy.release()
+		return reply
 	def sofia_hash(self, password):
 		s = ""
 		md5 = hashlib.md5(password).digest()
@@ -95,6 +103,31 @@ class DVRIPCam(object):
 		self.close()
 	def pretty_print(self, data):
 		print json.dumps(data, indent = 4, sort_keys = True)
+	def setAlarm(self, func):
+		self.alarm_func = func
+	def clearAlarm(self):
+		self.alarm_func = None
+	def alarmStart(self):
+		self.alarm = threading.Thread(name="DVRAlarm",target=self.alarm_thread, args=[self.busy])
+		self.alarm.start()
+		return self.get(1500,"")
+	def alarm_thread(self, event):
+		while True:
+			event.acquire()
+			try:
+				head, version, session, sequence_number, msgid, len_data = struct.unpack('BB2xII2xHI',self.socket.recv(20))
+				sleep(.1)#Just for recive whole packet
+				reply = self.socket.recv(len_data)
+				self.packet_count += 1
+				reply = json.loads(reply[:-2],encoding="utf8")
+				if msgid == 1504 and self.session == session:
+					if self.alarm_func != None: self.alarm_func(reply[reply['Name']], sequence_number)
+			except:
+				pass
+			finally:
+				event.release()
+			if self.socket == None:
+				break
 	def keep_alive(self):
 		self.send(self.QCODES["KeepAlive"],{"Name":"KeepAlive","SessionID":"0x%08X"%self.session})
 		self.alive = threading.Timer(self.alive_time, self.keep_alive)
@@ -107,7 +140,7 @@ class DVRIPCam(object):
 		return self.get(1042, command)
 	def get(self, code, command):
 		data = self.send(code, {"Name":str(command),"SessionID":"0x%08X"%self.session})
-		if data["Ret"] in self.OK_CODES:
+		if data["Ret"] in self.OK_CODES and data.has_key(str(command)):
 			return data[str(command)]
 		else:
 			return data
